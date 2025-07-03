8a9e950d36860c9597764bdbb33eeae7fe895596
fu-home <2152291484@qq.com>
2025-07-03 21:23:28 +0800

    "feat: add csapp.c in book_code dir"

--- book_code/csapp.h
+++ book_code/csapp.h
@@ -1,14 +1,19 @@
+/*
+ * csapp.h - prototypes and definitions for the CS:APP3e book
+ */
 /* $begin csapp.h */
 #ifndef __CSAPP_H__
 #define __CSAPP_H__
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <dirent.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -23,19 +28,18 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-
+ 
 /* Default file permissions are DEF_MODE & ~DEF_UMASK */
 /* $begin createmasks */
 #define DEF_MODE   S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH
 #define DEF_UMASK  S_IWGRP|S_IWOTH
 /* $end createmasks */
-
+ 
 /* Simplifies calls to bind(), connect(), and accept() */
 /* $begin sockaddrdef */
 typedef struct sockaddr SA;
 /* $end sockaddrdef */
-
+ 
 /* Persistent state for the robust I/O (Rio) package */
 /* $begin rio_t */
 #define RIO_BUFSIZE 8192
@@ -46,22 +50,23 @@ typedef struct {
     char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
 } rio_t;
 /* $end rio_t */
-
+ 
 /* External variables */
 extern int h_errno;    /* Defined by BIND for DNS errors */ 
 extern char **environ; /* Defined by libc */
-
+ 
 /* Misc constants */
 #define	MAXLINE	 8192  /* Max text line length */
 #define MAXBUF   8192  /* Max I/O buffer size */
 #define LISTENQ  1024  /* Second argument to listen() */
-
+ 
 /* Our own error-handling functions */
 void unix_error(char *msg);
 void posix_error(int code, char *msg);
 void dns_error(char *msg);
+void gai_error(int code, char *msg);
 void app_error(char *msg);
-
+ 
 /* Process control wrappers */
 pid_t Fork(void);
 void Execve(const char *filename, char *const argv[], char *const envp[]);
@@ -73,7 +78,7 @@ void Pause(void);
 unsigned int Alarm(unsigned int seconds);
 void Setpgid(pid_t pid, pid_t pgid);
 pid_t Getpgrp();
-
+ 
 /* Signal wrappers */
 typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
@@ -83,7 +88,18 @@ void Sigfillset(sigset_t *set);
 void Sigaddset(sigset_t *set, int signum);
 void Sigdelset(sigset_t *set, int signum);
 int Sigismember(const sigset_t *set, int signum);
-
+int Sigsuspend(const sigset_t *set);
+ 
+/* Sio (Signal-safe I/O) routines */
+ssize_t sio_puts(char s[]);
+ssize_t sio_putl(long v);
+void sio_error(char s[]);
+ 
+/* Sio wrappers */
+ssize_t Sio_puts(char s[]);
+ssize_t Sio_putl(long v);
+void Sio_error(char s[]);
+ 
 /* Unix I/O wrappers */
 int Open(const char *pathname, int flags, mode_t mode);
 ssize_t Read(int fd, void *buf, size_t count);
@@ -95,11 +111,16 @@ int Select(int  n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
 int Dup2(int fd1, int fd2);
 void Stat(const char *filename, struct stat *buf);
 void Fstat(int fd, struct stat *buf) ;
-
+ 
+/* Directory wrappers */
+DIR *Opendir(const char *name);
+struct dirent *Readdir(DIR *dirp);
+int Closedir(DIR *dirp);
+ 
 /* Memory mapping wrappers */
 void *Mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
 void Munmap(void *start, size_t length);
-
+ 
 /* Standard I/O wrappers */
 void Fclose(FILE *fp);
 FILE *Fdopen(int fd, const char *type);
@@ -108,13 +129,13 @@ FILE *Fopen(const char *filename, const char *mode);
 void Fputs(const char *ptr, FILE *stream);
 size_t Fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 void Fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
-
+ 
 /* Dynamic storage allocation wrappers */
 void *Malloc(size_t size);
 void *Realloc(void *ptr, size_t size);
 void *Calloc(size_t nmemb, size_t size);
 void Free(void *ptr);
-
+ 
 /* Sockets interface wrappers */
 int Socket(int domain, int type, int protocol);
 void Setsockopt(int s, int level, int optname, const void *optval, int optlen);
@@ -122,11 +143,20 @@ void Bind(int sockfd, struct sockaddr *my_addr, int addrlen);
 void Listen(int s, int backlog);
 int Accept(int s, struct sockaddr *addr, socklen_t *addrlen);
 void Connect(int sockfd, struct sockaddr *serv_addr, int addrlen);
-
+ 
+/* Protocol independent wrappers */
+void Getaddrinfo(const char *node, const char *service, 
+                 const struct addrinfo *hints, struct addrinfo **res);
+void Getnameinfo(const struct sockaddr *sa, socklen_t salen, char *host, 
+                 size_t hostlen, char *serv, size_t servlen, int flags);
+void Freeaddrinfo(struct addrinfo *res);
+void Inet_ntop(int af, const void *src, char *dst, socklen_t size);
+void Inet_pton(int af, const char *src, void *dst); 
+ 
 /* DNS wrappers */
 struct hostent *Gethostbyname(const char *name);
 struct hostent *Gethostbyaddr(const char *addr, int len, int type);
-
+ 
 /* Pthreads thread control wrappers */
 void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp, 
 		    void * (*routine)(void *), void *argp);
@@ -136,33 +166,34 @@ void Pthread_detach(pthread_t tid);
 void Pthread_exit(void *retval);
 pthread_t Pthread_self(void);
 void Pthread_once(pthread_once_t *once_control, void (*init_function)());
-
+ 
 /* POSIX semaphore wrappers */
 void Sem_init(sem_t *sem, int pshared, unsigned int value);
 void P(sem_t *sem);
 void V(sem_t *sem);
-
+ 
 /* Rio (Robust I/O) package */
 ssize_t rio_readn(int fd, void *usrbuf, size_t n);
 ssize_t rio_writen(int fd, void *usrbuf, size_t n);
 void rio_readinitb(rio_t *rp, int fd); 
 ssize_t	rio_readnb(rio_t *rp, void *usrbuf, size_t n);
 ssize_t	rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
-
+ 
 /* Wrappers for Rio package */
 ssize_t Rio_readn(int fd, void *usrbuf, size_t n);
 void Rio_writen(int fd, void *usrbuf, size_t n);
 void Rio_readinitb(rio_t *rp, int fd); 
 ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n);
 ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
-
-/* Client/server helper functions */
-int open_clientfd(char *hostname, int portno);
-int open_listenfd(int portno);
-
-/* Wrappers for client/server helper functions */
-int Open_clientfd(char *hostname, int port);
-int Open_listenfd(int port); 
-
+ 
+/* Reentrant protocol-independent client/server helpers */
+int open_clientfd(char *hostname, char *port);
+int open_listenfd(char *port);
+ 
+/* Wrappers for reentrant protocol-independent client/server helpers */
+int Open_clientfd(char *hostname, char *port);
+int Open_listenfd(char *port);
+ 
+ 
 #endif /* __CSAPP_H__ */
 /* $end csapp.h */
